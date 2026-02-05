a0f99a6e7ac5b05fdfce175f2ed2e3942dc8be34
saravana020705 <loganathan.t2023@vitstudent.ac.in>
2026-02-05 13:06:57 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from CI Pipeline!");
+        System.out.println("Hello from CI Pipeline!")
     }
 }
